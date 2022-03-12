a10bf179e0ff5b38d5259369ca13c8914693838e
FirdatulFitriyah <firdafitriyah585@gmail.com>
2022-03-12 11:32:09 +0700

    Menambahkan file variable.php

---
+++ variable.php
@@ -0,0 +1,30 @@
+<?php
+//Operator Aritmatika
+$a = 5;
+$b = 3;
+$c = 10;
+
+// penjumlahan
+$hasil = $a + $c;
+echo "Penjumlahan nilai $a + $c = $hasil";
+echo "<br>";
+$hasil = $b + $a + $c;
+echo "Penjumlahan nilai $b + $a + $c = $hasil";
+echo "<br>";
+
+// pengurangan
+$hasil = $c - $b;
+echo "Pengurangan nilai $c - $b = $hasil";
+echo "<br>";
+$hasil = $b - $a;
+echo "Pengurangan nilai $b - $a = $hasil";
+echo "<br>";
+
+// Perkalian
+$hasil = $c * $c;
+echo "Perkalian nilai $c * $c = $hasil";
+echo "<br>";
+$hasil = $a * $b;
+echo "Perkalian nilai $a * $b = $hasil";
+echo "<br>";
+?>
